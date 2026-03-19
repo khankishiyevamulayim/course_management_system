5bb8c786d4ca96f487dcc1f75a72c63ff416b510
khankishiyevamulayim <xankisiyevamulayim@gmail.com>
2026-03-19 11:29:41 +0400

    Course Management System ilkin nizamlamalar

--- src/main/java/org/example/coursemanagementsystem/config/KafkaConfig.java
+++ src/main/java/org/example/coursemanagementsystem/config/KafkaConfig.java
@@ -3,6 +3,7 @@ package org.example.coursemanagementsystem.config;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.example.coursemanagementsystem.dto.kafka.UserCreatedEvent;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -13,15 +14,23 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 import java.util.HashMap;
 import java.util.Map;
 
+
 @Configuration
 public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
 
     @Bean
     public ProducerFactory<String, UserCreatedEvent> producerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        // Bunu əlavə et — type header-i deaktiv edir
+        config.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+
         return new DefaultKafkaProducerFactory<>(config);
     }
 
